8d508141af4a62d98d3639715623292fb300c17c
amitmahato <073bct505.amit@pcampus.edu.np>
2019-10-26 16:34:14 +0545

    using tab navigation instead strategy to use different screens

--- navigator/AppNavigator.js
+++ navigator/AppNavigator.js
@@ -1,8 +1,16 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import HomeScreen from "../screens/HomeScreen";
+import SectionScreen from "../screens/SectionScreen";
+import TabNavigator from "./TabNavigator";
 
-const AppNavigator = createStackNavigator({
-  Home: HomeScreen
-});
-export default createAppContainer(AppNavigator);
+const AppNavigator = createStackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Section: { screen: SectionScreen }
+  },
+  {
+    mode: "modal"
+  }
+);
+export default createAppContainer(TabNavigator);
---
+++ navigator/TabNavigator.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import HomeScreen from "../screens/HomeScreen";
+import SectionScreen from "../screens/SectionScreen";
+import { Ionicons } from "@expo/vector-icons";
+import CoursesScreen from "../screens/CoursesScreen";
+import ProjectsScreen from "../screens/ProjectsScreen";
+
+const activeColor = "#4775f2";
+const inactiveColor = "#b8bece";
+
+const HomeStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Section: SectionScreen
+  },
+  {
+    mode: "modal"
+  }
+);
+
+HomeStack.navigationOptions = ({ navigation }) => {
+  const routeName = navigation.state.routes[navigation.state.index].routeName;
+
+  var tabBarVisible = routeName == "Section" ? false : true;
+
+  return {
+    tabBarVisible: tabBarVisible,
+    tabBarLabel: "Home",
+    tabBarIcon: ({ focused }) => (
+      <Ionicons
+        name="ios-home"
+        size={26}
+        color={focused ? activeColor : inactiveColor}
+      />
+    )
+  };
+};
+
+const CoursesStack = createStackNavigator({
+  Courses: CoursesScreen
+});
+
+CoursesStack.navigationOptions = {
+  tabBarLabel: "Courses",
+  tabBarIcon: ({ focused }) => (
+    <Ionicons
+      name="ios-albums"
+      size={26}
+      color={focused ? activeColor : inactiveColor}
+    />
+  )
+};
+
+const ProjectsStack = createStackNavigator({
+  Projects: ProjectsScreen
+});
+
+ProjectsStack.navigationOptions = {
+  tabBarLabel: "Projects",
+  tabBarIcon: ({ focused }) => (
+    <Ionicons
+      name="ios-folder"
+      size={26}
+      color={focused ? activeColor : inactiveColor}
+    />
+  )
+};
+
+const TabNavigator = createBottomTabNavigator({
+  HomeStack,
+  CoursesStack,
+  ProjectsStack
+});
+
+export default TabNavigator;
