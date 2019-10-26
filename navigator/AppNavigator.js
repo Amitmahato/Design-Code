20ed3ba9d6af5cdd90df52a34e24d9386853dfd3
amitmahato <073bct505.amit@pcampus.edu.np>
2019-10-26 14:43:26 +0545

    various errors that came with react-navigation were resolved by adding some additional dependencies

--- navigator/AppNavigator.js
+++ navigator/AppNavigator.js
@@ -1,4 +1,5 @@
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
 import HomeScreen from "../screens/HomeScreen";
 
 const AppNavigator = createStackNavigator({
