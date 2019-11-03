8faefda04dba059f39f497ff500cb8ff2db35b78
amitmahato <073bct505.amit@pcampus.edu.np>
2019-11-03 11:44:46 +0545

    created component for loading animation

---
+++ components/Loading.js
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import LottieView from "lottie-react-native";
+import { Animated, Dimensions } from "react-native";
+
+const screenHeight = Dimensions.get("window").height;
+
+export default class Loading extends React.Component {
+  state = {
+    top: new Animated.Value(0),
+    opacity: new Animated.Value(0)
+  };
+
+  componentDidUpdate() {
+    if (this.props.isActive) {
+      Animated.timing(this.state.top, { toValue: 0, duration: 0 }).start();
+      Animated.timing(this.state.opacity, { toValue: 1 }).start();
+      this.animation.play();
+    } else {
+      Animated.timing(this.state.top, {
+        toValue: screenHeight,
+        duration: 0
+      }).start();
+      Animated.timing(this.state.opacity, { toValue: 0 }).start();
+      this.animation.loop = false;
+    }
+  }
+
+  render() {
+    return (
+      <AnimatedContainer
+        style={{ top: this.state.top, opacity: this.state.opacity }}
+      >
+        <LottieView
+          source={require("../assets/lottie-loading.json")}
+          autoPlay={false}
+          loop={true}
+          ref={animation => {
+            this.animation = animation;
+          }}
+        />
+      </AnimatedContainer>
+    );
+  }
+}
+
+const Container = styled.View`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  background: rgba(255, 255, 255, 0.8);
+`;
+
+const AnimatedContainer = Animated.createAnimatedComponent(Container);
